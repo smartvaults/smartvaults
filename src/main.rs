80a98bed42b9fdd2cca9e3698daa16f1ab7e4a4e
Max Gravitt <max@digitalscarcity.io>
2023-01-18 08:53:09 -0500

    schnorr keys for nostr users matching BDK users

--- src/main.rs
+++ src/main.rs
@@ -7,44 +7,13 @@ use std::{env,
     thread,
 };
 extern crate serde_json;
+mod users;
 
-/*  Alice
-Secret Key (HEX): "d15949d0e9f2f06080f2ed40a115bac037f3dce28cbdeda52c168b4f374b01ea" 
-Public Key (HEX): "5c5ef19b5bd5c74a6826c27dca599d66623c2c4da423a7b8c42a1fc8f04b945e" 
-Public Key (bech32): "npub1t300rx6m6hr556pxcf7u5kvave3rctzd5s360wxy9g0u3uztj30qwelvqe" 
-Secret Key (bech32): "nsec169v5n58f7tcxpq8ja4q2z9d6cqml8h8z3j77mffvz6957d6tq84qjfym6p" 
-*/
-fn alice_keys() -> (String, String) {
-    ("d15949d0e9f2f06080f2ed40a115bac037f3dce28cbdeda52c168b4f374b01ea".to_string(),
-    "5c5ef19b5bd5c74a6826c27dca599d66623c2c4da423a7b8c42a1fc8f04b945e".to_string())
-}
+fn dump_keys(sk_str: String) {
 
-/*  Bob 
-Secret Key (HEX): "f21e8748241b4fe5ca129a265866dba80387758c3cdfeb539648672e3c5b2621" 
-Public Key (HEX): "875788b34464a298081521442eae87fdb0463ead9dd26c5a11e280550fb14c34" 
-Public Key (bech32): "npub1satc3v6yvj3fszq4y9zzat58lkcyv04dnhfxcks3u2q92ra3fs6qmgvwxe" 
-Secret Key (bech32): "nsec17g0gwjpyrd87tjsjngn9sekm4qpcwavv8n07k5ukfpnju0zmycsswzpe32" 
-*/
-fn bob_keys() -> (String, String) {
-    ("f21e8748241b4fe5ca129a265866dba80387758c3cdfeb539648672e3c5b2621".to_string(),
-    "875788b34464a298081521442eae87fdb0463ead9dd26c5a11e280550fb14c34".to_string())
-}
+    let sk = secret_key_from_str(&sk_str);
 
-/*  Charlie 
-Secret Key (HEX): "11710748a7e6774514b781f8328d030d2f74ad3cf4a79c244c12e53b110d9852" 
-Public Key (HEX): "c05ffa62d26beb3fefc336fc95b7669f8890072db78930f9f8ad4838d9d9560f" 
-Public Key (bech32): "npub1cp0l5ckjd04nlm7rxm7ftdmxn7yfqpedk7ynp70c44yr3kwe2c8sxkavnz" 
-Secret Key (bech32): "nsec1z9cswj98uem5299hs8ur9rgrp5hhftfu7jnecfzvztjnkygdnpfq4t03q9" 
-*/
-fn charlie_keys() -> (String, String) {
-    ("11710748a7e6774514b781f8328d030d2f74ad3cf4a79c244c12e53b110d9852".to_string(),
-    "c05ffa62d26beb3fefc336fc95b7669f8890072db78930f9f8ad4838d9d9560f".to_string())
-}
-
-fn _random_account() -> (String, String) {
-    let (secret_key_1, _public_key_1) = get_random_secret_key();
-
-    let (secret_str_1, public_str_1) = get_str_keys_from_secret(&secret_key_1);
+    let (secret_str_1, public_str_1) = get_str_keys_from_secret(&sk.unwrap());
 
     println!("Secret Key (HEX): {:?} ", secret_str_1); 
     println!("Public Key (HEX): {:?} ", public_str_1); 
@@ -55,6 +24,13 @@ fn _random_account() -> (String, String) {
     println!("Public Key (bech32): {:?} ", bech32_pub.unwrap());
     println!("Secret Key (bech32): {:?} ", bech32_prv.unwrap());
 
+}
+
+fn _random_account() -> (String, String) {
+    let (secret_key_1, _public_key_1) = get_random_secret_key();
+
+    let (secret_str_1, public_str_1) = get_str_keys_from_secret(&secret_key_1);
+    dump_keys(secret_str_1.clone());
     (secret_str_1, public_str_1)
 }
 
@@ -68,18 +44,18 @@ fn handle_message(message: &Message) -> Result<(), String> {
 fn subscribe(nostr_client: Arc<Mutex<Client>>) {
     
      // Run a new thread to handle messages
-     let subscription_id = nostr_client
+     let _subscription_id = nostr_client
      .lock()
      .unwrap()
      .subscribe(vec![ReqFilter {
          ids: None,
          authors: Some(vec![
-            alice_keys().1, bob_keys().1, charlie_keys().1
+            users::alice_keys().1, users::bob_keys().1, users::charlie_keys().1   //, elephant_keys().1
          ]),
          kinds: None,
          e: None,
          p: None,
-         since: None,
+         since: Some(1673908031),
          until: None,
          limit: Some(10),
      }])
@@ -98,9 +74,7 @@ fn subscribe(nostr_client: Arc<Mutex<Client>>) {
         }
     });
 
-
      handle_thread.join().unwrap();
-
 }
 
 
@@ -110,97 +84,42 @@ fn main()  {
     dbg!(&args);
 
     let nostr_client = Arc::new(Mutex::new(
-        Client::new(vec!["ws://127.0.0.1:8080"]).unwrap(),
+        Client::new(vec!["ws://127.0.0.1:8081"]).unwrap(),
     ));
 
     if &args.len() > &1 && args[1] == "subscribe".to_string() {
         subscribe(nostr_client);
     } else if args[1] == "post".to_string() {
-        // coinstr post bob "hello everyone"
+
         let mut prv_key = "".to_string();
         if args[2] == "bob" {
-            prv_key = bob_keys().0;
+            prv_key = users::bob_keys().0;
         } else if args[2] == "alice" {
-            prv_key = alice_keys().0;
+            prv_key = users::alice_keys().0;
         } else if args[2] == "charlie" {
-            prv_key = charlie_keys().0;
-        }
-        let message = &args[3];
+            prv_key = users::charlie_keys().0;
+        } 
 
+        let message = &args[3];
         let poster_identity = Identity::from_str(&prv_key).unwrap();
-        
         nostr_client
             .lock()
             .unwrap()
             .publish_text_note(&poster_identity, &message, &[], 0)
             .unwrap();
 
+    } else if args[1] == "key".to_string() {
+        dump_keys(args[2].to_string());
+    } else if args[1] == "dump".to_string() {
+        dump_keys(users::alice_keys().0);
+        dump_keys(users::bob_keys().0);
+        dump_keys(users::charlie_keys().0);
     }
-
-
-    // 
-
-    // let (secret_str_1, public_str_1 ) = random_account(); // random_account();
-
-    
-
-
-
-    // // Run a new thread to handle messages
-    // let nostr_clone = nostr_client.clone();
-    // let handle_thread = thread::spawn(move || {
-    //     println!("Listening...");
-    //     let events = nostr_clone.lock().unwrap().next_data().unwrap();
-
-    //     for (relay_url, message) in events.iter() {
-    //         handle_message(relay_url, message).unwrap();
-    //     }
-    // });
-
-     // Change metadata
-    //  nostr_client
-    //  .lock()
-    //  .unwrap()
-    //  .set_metadata(
-    //      &my_identity,
-    //      Some("Rust Nostr Client test account"),
-    //      Some("Hello Nostr! #5"),
-    //      None,
-    //      None,
-    //      0,
-    //  )
-    //  .unwrap();
-
-    //  let subscription_id = nostr_client
-    //  .lock()
-    //  .unwrap()
-    //  .subscribe(vec![ReqFilter {
-    //      ids: None,
-    //      authors: Some(vec![
-    //         alice_keys().1, bob_keys().1, charlie_keys().1
-    //      ]),
-    //      kinds: None,
-    //      e: None,
-    //      p: None,
-    //      since: None,
-    //      until: None,
-    //      limit: Some(1),
-    //  }])
-    //  .unwrap();
-
-    //  nostr_client
-    //     .lock()
-    //     .unwrap()
-    //     .publish_text_note(&my_identity, "bamboozler :)", &[], 0)
-    //     .unwrap();
-
-    // // Unsubscribe
-    // nostr_client
-    //     .lock()
-    //     .unwrap()
-    //     .unsubscribe(&subscription_id)
-    //     .unwrap();
-
-    // handle_thread.join().unwrap();
+ 
 }
 
+// basic 2 of 3 multisig with Alice Bob and Charlie
+/* thresh(2,pk(cPatMiTiN4gWsBQpKuHPY2d3Z41NWGu2xEvNumubhPADh7VHzqqV),
+    pk(02476b018f75b1084e4b2bd652a747a37de9727183bcfe4113fe0b9390767e3543),
+    pk(023254bcb92a82208ac8d864f3772c1576eb12dd97f1110d858cedb58251ba5043))
+*/
---
+++ src/users.rs
@@ -0,0 +1,33 @@
+
+/*  Alice
+Secret Key (HEX): "3bc51062973c458d5a6f2d8d64a023246354ad7e064b1e4e009ec8a0699a3043" 
+Public Key (HEX): "7e5ccd015578969febb42468f8d0be54c6b39331b7285d88040d5f0ba9606aa4" 
+Public Key (bech32): "npub10ewv6q240ztfl6a5y35035972nrt8ye3ku59mzqyp40sh2tqd2jqveljzy" 
+Secret Key (bech32): "nsec180z3qc5h83zc6kn09kxkfgpry334ftt7qe93unsqnmy2q6v6xppsv4ck26" 
+*/
+pub fn alice_keys() -> (String, String) {
+    ("3bc51062973c458d5a6f2d8d64a023246354ad7e064b1e4e009ec8a0699a3043".to_string(), // updated
+    "7e5ccd015578969febb42468f8d0be54c6b39331b7285d88040d5f0ba9606aa4".to_string())
+}
+
+/*  Bob 
+Secret Key (HEX): "cd9fb1e148ccd8442e5aa74904cc73bf6fb54d1d54d333bd596aa9bb4bb4e961" 
+Public Key (HEX): "476b018f75b1084e4b2bd652a747a37de9727183bcfe4113fe0b9390767e3543" 
+Public Key (bech32): "npub1ga4srrm4kyyyujet6ef2w3ar0h5hyuvrhnlyzyl7pwfeqan7x4psmtdtkk" 
+Secret Key (bech32): "nsec1ek0mrc2genvygtj65aysfnrnhahm2nga2nfn802ed25mkja5a9sstwpm9k" 
+*/
+pub fn bob_keys() -> (String, String) {
+    ("cd9fb1e148ccd8442e5aa74904cc73bf6fb54d1d54d333bd596aa9bb4bb4e961".to_string(),
+    "476b018f75b1084e4b2bd652a747a37de9727183bcfe4113fe0b9390767e3543".to_string())
+}
+
+/*  Charlie 
+Secret Key (HEX): "6e81b1255ad51bb201a2b8afa9b66653297ae0217f833b14b39b5231228bf968" 
+Public Key (HEX): "3254bcb92a82208ac8d864f3772c1576eb12dd97f1110d858cedb58251ba5043" 
+Public Key (bech32): "npub1xf2tewf2sgsg4jxcvnehwtq4wm439hvh7ygsmpvvak6cy5d62ppsk84lf4" 
+Secret Key (bech32): "nsec1d6qmzf2665dmyqdzhzh6ndnx2v5h4cpp07pnk99nndfrzg5tl95qwfu7cz" 
+*/
+pub fn charlie_keys() -> (String, String) {
+    ("6e81b1255ad51bb201a2b8afa9b66653297ae0217f833b14b39b5231228bf968".to_string(),
+    "3254bcb92a82208ac8d864f3772c1576eb12dd97f1110d858cedb58251ba5043".to_string())
+}
