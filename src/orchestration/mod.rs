8af0b69562061b9cf07f48d492a1faa118887195
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-03-09 18:36:28 +0100

    Update `CoinstrPolicy` struct

--- src/orchestration/mod.rs
+++ src/orchestration/mod.rs
@@ -13,9 +13,12 @@ impl Maestro {
 #[cfg(test)]
 mod tests {
 	use bdk::{
+		bitcoin::Network,
 		blockchain::ElectrumBlockchain,
+		database::MemoryDatabase,
 		electrum_client::Client,
 		wallet::{AddressIndex::New, SyncOptions},
+		Wallet,
 	};
 
 	use crate::{policy::CoinstrPolicy, user::User, DEFAULT_RELAY, DEFAULT_TESTNET_ENDPOINT};
@@ -36,30 +39,29 @@ mod tests {
 			"A 1 of 2 Taptree policy".to_string(),
 			&alice,
 			&bob,
-		);
-		println!("{}", &policy.as_ref().unwrap());
+		)
+		.unwrap();
+		println!("{policy}");
 
 		println!("Syncing policy wallet.");
+		let database = MemoryDatabase::new();
 		let blockchain = ElectrumBlockchain::from(Client::new(DEFAULT_TESTNET_ENDPOINT).unwrap());
-		policy
-			.as_ref()
-			.unwrap()
-			.wallet
-			.sync(&blockchain, SyncOptions::default())
-			.unwrap();
+		let wallet =
+			Wallet::new(&policy.descriptor.to_string(), None, Network::Testnet, database).unwrap();
+		wallet.sync(&blockchain, SyncOptions::default()).unwrap();
 
-		let balance = policy.as_ref().unwrap().wallet.get_balance().unwrap();
+		let balance = wallet.get_balance().unwrap();
 		println!("Wallet balances in SATs: {}", balance);
 
 		const TEST_NUM_SATS: u64 = 500;
 		if balance.confirmed < TEST_NUM_SATS {
-			let receiving_address = &policy.unwrap().wallet.get_address(New).unwrap();
+			let receiving_address = wallet.get_address(New).unwrap();
 			println!("Refill this testnet wallet from the faucet: 	https://bitcoinfaucet.uo1.net/?to={receiving_address}");
 			return;
 		}
 
 		let (mut psbt, tx_details) = {
-			let mut builder = policy.as_ref().unwrap().wallet.build_tx();
+			let mut builder = wallet.build_tx();
 			builder.add_recipient(alice_address.script_pubkey(), TEST_NUM_SATS);
 			builder.finish().unwrap()
 		};
@@ -77,7 +79,7 @@ mod tests {
 			.publish_text_note(psbt.to_string(), &[bob_tag])
 			.expect("cannot publish note");
 
-		let receiving_address = &policy.unwrap().wallet.get_address(New).unwrap();
+		let receiving_address = wallet.get_address(New).unwrap();
 		println!("Refill this testnet wallet from the faucet: 	https://bitcoinfaucet.uo1.net/?to={receiving_address}");
 	}
 
