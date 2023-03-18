aabbcc26a6f0a7a5c2de2c7f33a270d27a3995bc
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-03-18 11:13:54 +0100

    core: move extract public keys example to tests

--- coinstr-core/src/util.rs
+++ coinstr-core/src/util.rs
@@ -24,3 +24,32 @@ where
     }
     Ok(public_keys)
 }
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn test_extract_public_keys() {
+        let descriptor = "thresh(2,pk(02e69d88524a5669723b473523cd2c6bfe76d6c289656c3ecd7981fa8fef784dcc),pk(02101e7953a54b18d0f41ea199b9adf2d7e643441b5af8e539531e6d7275cee1df),pk(02ea527e059759d368a55253270454e58e9d6e4fe2e98d302d6e01821fa973259d))";
+        let pubkeys = extract_public_keys(descriptor).unwrap();
+
+        assert_eq!(
+            pubkeys,
+            vec![
+                XOnlyPublicKey::from_str(
+                    "e69d88524a5669723b473523cd2c6bfe76d6c289656c3ecd7981fa8fef784dcc"
+                )
+                .unwrap(),
+                XOnlyPublicKey::from_str(
+                    "101e7953a54b18d0f41ea199b9adf2d7e643441b5af8e539531e6d7275cee1df"
+                )
+                .unwrap(),
+                XOnlyPublicKey::from_str(
+                    "ea527e059759d368a55253270454e58e9d6e4fe2e98d302d6e01821fa973259d"
+                )
+                .unwrap(),
+            ]
+        )
+    }
+}
