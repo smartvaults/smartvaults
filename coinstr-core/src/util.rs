a20cdf080279997ae0a7af4c5f64c9e5f55b6694
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-03-18 14:35:52 +0100

    core: improve `extract_public_keys` function

--- coinstr-core/src/util.rs
+++ coinstr-core/src/util.rs
@@ -3,6 +3,9 @@ use std::str::FromStr;
 use bdk::bitcoin::XOnlyPublicKey;
 pub use keechain_core::util::*;
 
+const PUBLIC_KEY_LEN: usize = 66;
+const HEX_CHARS: &str = "ABCDEFabcdef0123456789";
+
 #[derive(Debug, thiserror::Error)]
 pub enum Error {
     #[error(transparent)]
@@ -14,15 +17,35 @@ where
     S: Into<String>,
 {
     let descriptor: String = descriptor.into();
-    let splitted = descriptor.split("pk(");
+    let len: usize = descriptor.len();
     let mut public_keys: Vec<XOnlyPublicKey> = Vec::new();
-    for chunk in splitted.into_iter() {
-        if let Some(pubkey) = chunk.get(2..66) {
-            let pubkey = XOnlyPublicKey::from_str(pubkey)?;
-            public_keys.push(pubkey);
+    for (index, _char) in descriptor.char_indices() {
+        if len - index < PUBLIC_KEY_LEN {
+            break;
+        }
+        if let Some(chunk) = descriptor.get(index..index + PUBLIC_KEY_LEN) {
+            if maybe_pubkey(chunk) {
+                if let Ok(pubkey) = XOnlyPublicKey::from_str(&chunk[2..]) {
+                    public_keys.push(pubkey);
+                }
+            }
         }
     }
     Ok(public_keys)
+}
+
+fn maybe_pubkey(chunk: &str) -> bool {
+    if chunk.len() != 66 {
+        return false;
+    }
+
+    for c in chunk.chars() {
+        if !HEX_CHARS.contains(c) {
+            return false;
+        }
+    }
+
+    true
 }
 
 #[cfg(test)]
@@ -30,7 +53,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn test_extract_public_keys() {
+    fn test_policy_extractor() {
         let descriptor = "thresh(2,pk(02e69d88524a5669723b473523cd2c6bfe76d6c289656c3ecd7981fa8fef784dcc),pk(02101e7953a54b18d0f41ea199b9adf2d7e643441b5af8e539531e6d7275cee1df),pk(02ea527e059759d368a55253270454e58e9d6e4fe2e98d302d6e01821fa973259d))";
         let pubkeys = extract_public_keys(descriptor).unwrap();
 
@@ -47,6 +70,30 @@ mod test {
                 .unwrap(),
                 XOnlyPublicKey::from_str(
                     "ea527e059759d368a55253270454e58e9d6e4fe2e98d302d6e01821fa973259d"
+                )
+                .unwrap(),
+            ]
+        )
+    }
+
+    #[test]
+    fn test_descriptor_extractor() {
+        let descriptor = "wsh(multi(2,02e69d88524a5669723b473523cd2c6bfe76d6c289656c3ecd7981fa8fef784dcc,02101e7953a54b18d0f41ea199b9adf2d7e643441b5af8e539531e6d7275cee1df,027b9eda7669b1075c0eb4b117a34de19be4b3c8b0d5537b5de7fa9793b0a8e9ff))#lrsyq0eg";
+        let pubkeys = extract_public_keys(descriptor).unwrap();
+
+        assert_eq!(
+            pubkeys,
+            vec![
+                XOnlyPublicKey::from_str(
+                    "e69d88524a5669723b473523cd2c6bfe76d6c289656c3ecd7981fa8fef784dcc"
+                )
+                .unwrap(),
+                XOnlyPublicKey::from_str(
+                    "101e7953a54b18d0f41ea199b9adf2d7e643441b5af8e539531e6d7275cee1df"
+                )
+                .unwrap(),
+                XOnlyPublicKey::from_str(
+                    "7b9eda7669b1075c0eb4b117a34de19be4b3c8b0d5537b5de7fa9793b0a8e9ff"
                 )
                 .unwrap(),
             ]
