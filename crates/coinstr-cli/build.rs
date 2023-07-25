4b358b27bd13be6298f6d973d23d028da6d94b08
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-07-25 17:39:26 +0200

    Update `build.rs` scripts

--- crates/coinstr-cli/build.rs
+++ crates/coinstr-cli/build.rs
@@ -4,11 +4,10 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
-    let version = format!("v{}  {}", env!("CARGO_PKG_VERSION"), git_hash);
-    println!("cargo:rustc-env=CARGO_PKG_VERSION={version}");
+    if let Ok(output) = Command::new("git").args(["rev-parse", "HEAD"]).output() {
+        if let Ok(git_hash) = String::from_utf8(output.stdout) {
+            let version = format!("v{}  {}", env!("CARGO_PKG_VERSION"), git_hash);
+            println!("cargo:rustc-env=CARGO_PKG_VERSION={version}");
+        }
+    }
 }
--- crates/coinstr/build.rs
+++ crates/coinstr/build.rs
@@ -4,10 +4,9 @@
 use std::process::Command;
 
 fn main() {
-    let output = Command::new("git")
-        .args(["rev-parse", "HEAD"])
-        .output()
-        .unwrap();
-    let git_hash = String::from_utf8(output.stdout).unwrap();
-    println!("cargo:rustc-env=GIT_HASH={git_hash}");
+    if let Ok(output) = Command::new("git").args(["rev-parse", "HEAD"]).output() {
+        if let Ok(git_hash) = String::from_utf8(output.stdout) {
+            println!("cargo:rustc-env=GIT_HASH={git_hash}");
+        }
+    }
 }
