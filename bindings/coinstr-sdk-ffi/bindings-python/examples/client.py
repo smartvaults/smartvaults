9a11903c749db48c54bed33ea240a9f0822eb62a
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-07-26 15:11:03 +0200

    ffi: add `client.py` example

--- bindings/coinstr-sdk-ffi/bindings-python/examples/client.py
+++ bindings/coinstr-sdk-ffi/bindings-python/examples/client.py
@@ -3,17 +3,21 @@ import time
 
 coinstr = Coinstr.open("/home/user/.coinstr", "test", "test", Network.TESTNET)
 
+policies = []
+
 class SyncNotifications(SyncHandler):
     def handle(self):
         print("Refreshing...")
+        global policies
+        policies = coinstr.get_policies()
 
-coinstr.handle_sync(SyncNotifications())
-
-policies = coinstr.get_policies()
-
-for p in policies:
-    print(p.policy().name())
-
+handle = coinstr.handle_sync(SyncNotifications())
 
 while True:
     time.sleep(5.0)
+
+    for p in policies:
+        print(p.policy().name())
+
+    handle.abort()
+    print(f"Aborted: {handle.is_aborted()}")
