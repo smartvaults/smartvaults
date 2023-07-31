1ab413c2e0f02bbed3c08f0e535139660f85686b
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-07-31 12:57:06 +0200

    sdk: fix `save_label` method for `Coinstr`

--- crates/coinstr-sdk/src/client/label.rs
+++ crates/coinstr-sdk/src/client/label.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2022-2023 Coinstr
 // Distributed under the MIT software license
 
+use coinstr_core::bitcoin::XOnlyPublicKey;
 use nostr_sdk::{Event, EventBuilder, EventId, Keys, Tag};
 
 use super::{Coinstr, Error};
@@ -11,22 +12,26 @@ use crate::util::encryption::EncryptionWithKeys;
 impl Coinstr {
     pub async fn save_label(&self, policy_id: EventId, label: Label) -> Result<(), Error> {
         let shared_key: Keys = self.db.get_shared_key(policy_id)?;
+        let nostr_pubkeys: Vec<XOnlyPublicKey> = self.db.get_nostr_pubkeys(policy_id)?;
 
         // TODO: check if address or UTXO actually belong to the policy
 
-        // Compose label event
+        // Compose event
         let identifier: String = label.generate_identifier(&shared_key)?;
         let content: String = label.encrypt_with_keys(&shared_key)?;
-        let tags: Vec<Tag> = vec![
-            Tag::Identifier(identifier),
-            Tag::Event(policy_id, None, None),
-        ];
+        let mut tags: Vec<Tag> = nostr_pubkeys
+            .iter()
+            .map(|p| Tag::PubKey(*p, None))
+            .collect();
+        tags.push(Tag::Identifier(identifier.clone()));
+        tags.push(Tag::Event(policy_id, None, None));
         let event: Event = EventBuilder::new(LABELS_KIND, content, &tags).to_event(&shared_key)?;
 
         // Publish event
         self.send_event(event, Some(SEND_TIMEOUT)).await?;
 
-        // TODO: save in db
+        // Save to db
+        self.db.save_label(identifier, policy_id, label)?;
 
         Ok(())
     }
