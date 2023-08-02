a2b98b1e0dc5e0bc71515c00f19d946b33be9638
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-08-02 09:29:57 +0200

    gui: remove not needed const

--- crates/coinstr/src/constants.rs
+++ crates/coinstr/src/constants.rs
@@ -1,18 +1,9 @@
 // Copyright (c) 2022-2023 Coinstr
 // Distributed under the MIT software license
 
-//use coinstr_sdk::core::bitcoin::Network;
-
 pub const APP_NAME: &str = "Coinstr";
 pub const APP_LOGO_MAINNET: &[u8] = include_bytes!("../static/img/coinstr.svg");
 pub const APP_LOGO_TESTNET: &[u8] = include_bytes!("../static/img/coinstr-testnet.svg");
 pub const APP_LOGO_SIGNET: &[u8] = include_bytes!("../static/img/coinstr-signet.svg");
 pub const APP_LOGO_REGTEST: &[u8] = include_bytes!("../static/img/coinstr-regtest.svg");
 pub const APP_DESCRIPTION: &str = env!("CARGO_PKG_DESCRIPTION");
-
-/* pub const NETWORKS: [Network; 4] = [
-    Network::Bitcoin,
-    Network::Testnet,
-    Network::Signet,
-    Network::Regtest,
-]; */
