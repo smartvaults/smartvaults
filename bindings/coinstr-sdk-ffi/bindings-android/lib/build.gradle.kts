981ffedee69f35b4e6d0fbdbc1e4adad89716ea5
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-08-19 09:45:16 +0200

    ffi: add `android` publish scripts

--- bindings/coinstr-sdk-ffi/bindings-android/lib/build.gradle.kts
+++ bindings/coinstr-sdk-ffi/bindings-android/lib/build.gradle.kts
@@ -1,6 +1,11 @@
+// library version is defined in gradle.properties
+val libraryVersion: String by project
+
 plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android") version "1.6.10"
+    id("maven-publish")
+    id("signing")
 }
 
 repositories {
@@ -23,6 +28,13 @@ android {
             proguardFiles(file("proguard-android-optimize.txt"), file("proguard-rules.pro"))
         }
     }
+
+    publishing {
+        singleVariant("release") {
+            withSourcesJar()
+            withJavadocJar()
+        }
+    }
 }
 
 dependencies {
@@ -30,4 +42,46 @@ dependencies {
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7")
     implementation("androidx.appcompat:appcompat:1.4.0")
     implementation("androidx.core:core-ktx:1.7.0")
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            create<MavenPublication>("maven") {
+                groupId = "io.coinstr"
+                artifactId = "coinstr-sdk"
+                version = libraryVersion
+
+                from(components["release"])
+                pom {
+                    name.set("coinstr-sdk")
+                    description.set("Coinstr SDK Kotlin language bindings.")
+                    url.set("https://github.com/coinstr/coinstr")
+                    licenses {
+                        license {
+                            name.set("MIT")
+                            url.set("https://github.com/coinstr/coinstr/blob/master/LICENSE")
+                        }
+                    }
+                    developers {
+                        developer {
+                            id.set("yukibtc")
+                            name.set("Yuki Kishimoto")
+                            email.set("yukikishimoto@protonmail.com")
+                        }
+                    }
+                    scm {
+                        connection.set("scm:git:github.com/coinstr/coinstr.git")
+                        developerConnection.set("scm:git:ssh://github.com/coinstr/coinstr.git")
+                        url.set("https://github.com/coinstr/coinstr/tree/master")
+                    }
+                }
+            }
+        }
+    }
+}
+
+signing {
+    useGpgCmd()
+    sign(publishing.publications)
 }
