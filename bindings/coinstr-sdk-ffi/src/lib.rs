ccb9697231fa7dd8619747c654b0e2fd855dded5
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-09-07 15:45:35 +0200

    sdk: add `init_mobile` in `logger` module

--- bindings/coinstr-sdk-ffi/src/lib.rs
+++ bindings/coinstr-sdk-ffi/src/lib.rs
@@ -1,6 +1,8 @@
 // Copyright (c) 2022-2023 Coinstr
 // Distributed under the MIT software license
 
+use coinstr_sdk::logger;
+
 mod abortable;
 mod address;
 mod amount;
@@ -10,7 +12,6 @@ mod config;
 mod contact;
 mod descriptor;
 mod error;
-mod logger;
 mod message;
 mod network;
 mod nip46;
@@ -22,8 +23,53 @@ mod transaction;
 
 use self::error::Result;
 
+// Error
+pub use crate::error::FFIError;
+
+// External
+pub use coinstr_sdk::core::policy::PolicyTemplateType;
+pub use coinstr_sdk::core::signer::SignerType;
+pub use coinstr_sdk::core::types::WordCount;
+pub use coinstr_sdk::nostr::RelayStatus;
+pub use nostr_sdk_ffi::{
+    EventId, Keys, Metadata, NostrConnectURI, PublicKey, Relay, RelayConnectionStats,
+    RelayInformationDocument, SecretKey, Timestamp,
+};
+
+// Coinstr
+pub use crate::abortable::AbortHandle;
+pub use crate::address::{AddressIndex, GetAddress};
+pub use crate::amount::Amount;
+pub use crate::balance::Balance;
+pub use crate::client::{Coinstr, SyncHandler};
+pub use crate::config::Config;
+pub use crate::contact::GetContact;
+pub use crate::descriptor::Descriptor;
+pub use crate::message::{EventHandled, Message};
+pub use crate::network::Network;
+pub use crate::nip46::{NostrConnectRequest, NostrConnectSession};
+pub use crate::policy::{
+    AbsoluteLockTime, GetPolicy, Policy, PolicyTemplate, RecoveryTemplate, RelativeLockTime,
+};
+pub use crate::proposal::{
+    ApprovedProposal, CompletedProposal, GetApproval, GetCompletedProposal, GetProposal, Proposal,
+};
+pub use crate::seed::Seed as KeychainSeed;
+pub use crate::signer::{GetSharedSigner, GetSigner, SharedSigner, Signer};
+pub use crate::transaction::{
+    BlockTime, GetTransaction, OutPoint, Transaction, TransactionDetails, TxIn, TxOut, Utxo,
+};
+
 pub fn git_hash_version() -> String {
     env!("GIT_HASH").to_string()
+}
+
+pub fn init_desktop_logger(base_path: String, network: Network) -> Result<()> {
+    Ok(logger::init(base_path, network.into(), true)?)
+}
+
+pub fn init_mobile_logger() {
+    logger::init_mobile()
 }
 
 pub fn get_keychains_list(base_path: String, network: Network) -> Result<Vec<String>> {
@@ -33,50 +79,5 @@ pub fn get_keychains_list(base_path: String, network: Network) -> Result<Vec<Str
     )?)
 }
 
-mod ffi {
-    // Error
-    pub use crate::error::FFIError;
-
-    // External
-    pub use coinstr_sdk::core::policy::PolicyTemplateType;
-    pub use coinstr_sdk::core::signer::SignerType;
-    pub use coinstr_sdk::core::types::WordCount;
-    pub use coinstr_sdk::nostr::RelayStatus;
-    pub use nostr_sdk_ffi::{
-        EventId, Keys, Metadata, NostrConnectURI, PublicKey, Relay, RelayConnectionStats,
-        RelayInformationDocument, SecretKey, Timestamp,
-    };
-
-    // Namespace
-    pub use crate::logger::init_logger;
-    pub use crate::{get_keychains_list, git_hash_version};
-
-    // Coinstr
-    pub use crate::abortable::AbortHandle;
-    pub use crate::address::{AddressIndex, GetAddress};
-    pub use crate::amount::Amount;
-    pub use crate::balance::Balance;
-    pub use crate::client::{Coinstr, SyncHandler};
-    pub use crate::config::Config;
-    pub use crate::contact::GetContact;
-    pub use crate::descriptor::Descriptor;
-    pub use crate::message::{EventHandled, Message};
-    pub use crate::network::Network;
-    pub use crate::nip46::{NostrConnectRequest, NostrConnectSession};
-    pub use crate::policy::{
-        AbsoluteLockTime, GetPolicy, Policy, PolicyTemplate, RecoveryTemplate, RelativeLockTime,
-    };
-    pub use crate::proposal::{
-        ApprovedProposal, CompletedProposal, GetApproval, GetCompletedProposal, GetProposal,
-        Proposal,
-    };
-    pub use crate::seed::Seed as KeychainSeed;
-    pub use crate::signer::{GetSharedSigner, GetSigner, SharedSigner, Signer};
-    pub use crate::transaction::{
-        BlockTime, GetTransaction, OutPoint, Transaction, TransactionDetails, TxIn, TxOut, Utxo,
-    };
-
-    // UDL
-    uniffi::include_scaffolding!("coinstr_sdk");
-}
-pub use ffi::*;
+// UDL
+uniffi::include_scaffolding!("coinstr_sdk");
--- crates/coinstr-sdk/src/logger.rs
+++ crates/coinstr-sdk/src/logger.rs
@@ -14,7 +14,7 @@ use tracing_subscriber::fmt::writer::BoxMakeWriter;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::reload::Layer as ReloadLayer;
 use tracing_subscriber::util::{SubscriberInitExt, TryInitError};
-use tracing_subscriber::Layer;
+use tracing_subscriber::{fmt, Layer};
 
 use crate::util::dir;
 
@@ -28,6 +28,20 @@ pub enum Error {
     Logger(#[from] TryInitError),
 }
 
+fn targets_filter() -> Targets {
+    Targets::new()
+        .with_default(Level::WARN)
+        .with_target("bdk", Level::INFO)
+        .with_target("bdk::database::sqlite", Level::WARN)
+        .with_target("keechain_core", Level::INFO)
+        .with_target("nostr_sdk", Level::DEBUG)
+        .with_target("coinstr_core", Level::DEBUG)
+        .with_target("coinstr_sdk", Level::TRACE)
+        .with_target("coinstr", Level::DEBUG)
+        .with_target("coinstr_sdk_ffi", LevelFilter::OFF)
+}
+
+#[cfg(all(not(target_os = "android"), not(target_os = "ios")))]
 pub fn init<P>(base_path: P, network: Network, stdout: bool) -> Result<(), Error>
 where
     P: AsRef<Path>,
@@ -51,36 +65,52 @@ where
 
     let file_appender = tracing_appender::rolling::never(path, format!("{}.log", now.as_u64()));
     let writer = BoxMakeWriter::new(file_appender);
-    let file_log = tracing_subscriber::fmt::layer()
+    let file_log = fmt::layer()
         .with_writer(writer)
         .with_ansi(false)
         .with_file(false);
     let (file_log, ..) = ReloadLayer::new(file_log);
 
-    let target_filter = Targets::new()
-        .with_default(Level::WARN)
-        .with_target("bdk", Level::INFO)
-        .with_target("bdk::database::sqlite", Level::WARN)
-        .with_target("keechain_core", Level::INFO)
-        .with_target("nostr_sdk", Level::DEBUG)
-        .with_target("coinstr_core", Level::DEBUG)
-        .with_target("coinstr_sdk", Level::TRACE)
-        .with_target("coinstr", Level::DEBUG)
-        .with_target("coinstr_sdk_ffi", LevelFilter::OFF);
+    let targets_filter = targets_filter();
 
     if stdout {
-        let stdout_log = tracing_subscriber::fmt::layer()
-            .with_ansi(!cfg!(any(target_os = "android", target_os = "ios")))
+        let stdout_log = fmt::layer()
+            .with_ansi(true)
             .with_file(false)
             .with_span_events(FmtSpan::CLOSE);
         tracing_subscriber::registry()
-            .with(stdout_log.and_then(file_log).with_filter(target_filter))
+            .with(stdout_log.and_then(file_log).with_filter(targets_filter))
             .try_init()?;
     } else {
         tracing_subscriber::registry()
-            .with(file_log.with_filter(target_filter))
+            .with(file_log.with_filter(targets_filter))
             .try_init()?;
     };
 
     Ok(())
+}
+
+//#[cfg(any(target_os = "android", target_os = "ios"))]
+pub fn init_mobile() {
+    #[cfg(target_os = "android")]
+    let layer = fmt::layer().with_writer(paranoid_android::AndroidLogMakeWriter::new(
+        "io.coinstr.sdk".to_owned(),
+    ));
+
+    #[cfg(not(target_os = "android"))]
+    let layer = fmt::layer();
+
+    match tracing_subscriber::registry()
+        .with(
+            layer
+                .with_ansi(false)
+                .with_file(false)
+                .with_span_events(FmtSpan::CLOSE)
+                .with_filter(targets_filter()),
+        )
+        .try_init()
+    {
+        Ok(_) => tracing::info!("Logger initialized"),
+        Err(e) => eprintln!("Impossible to init logger: {e}"),
+    }
 }
