df9cb21310505fbc1d259de51105a15d65b65268
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-10-05 11:31:25 +0200

    protocol: use `Identifier` instead of `EventId` in `SharedKey`

--- crates/smartvaults-protocol/src/v2/shared_key.rs
+++ crates/smartvaults-protocol/src/v2/shared_key.rs
@@ -2,13 +2,13 @@
 // Distributed under the MIT software license
 
 use nostr::event::builder::Error;
-use nostr::{Event, EventBuilder, EventId, Keys, PublicKey, SecretKey, Tag};
+use nostr::{Event, EventBuilder, Keys, PublicKey, SecretKey, Tag};
 use serde::{Deserialize, Serialize};
 use smartvaults_core::bitcoin::Network;
 
 use super::constants::SHARED_KEY_KIND_V2;
 use super::crypto::{self, Version as CryptoVersion};
-use super::NetworkMagic;
+use super::{Identifier, NetworkMagic};
 
 #[derive(Debug, Clone, PartialEq, Eq, Serialize, Deserialize)]
 #[serde(rename_all = "snake_case")]
@@ -16,8 +16,8 @@ pub enum SharedKey {
     V1 {
         /// Secret key
         shared_key: SecretKey,
-        /// Event ID
-        policy_id: EventId,
+        /// Policy Identifier
+        policy_identifier: Identifier,
         /// Network magic
         network: NetworkMagic,
     },
@@ -27,13 +27,13 @@ pub fn build_event(
     keys: &Keys,
     receiver: &PublicKey,
     shared_key: &Keys,
-    policy_id: EventId,
+    policy_identifier: Identifier,
     network: Network,
 ) -> Result<Event, Error> {
     // Compose Shared Key
     let shared_key = SharedKey::V1 {
         shared_key: shared_key.secret_key()?.clone(),
-        policy_id,
+        policy_identifier,
         network: network.into(),
     };
 
