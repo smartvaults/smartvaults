d25a19d04416cbd3d26efec8adbeef85e850805d
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-10-17 14:08:21 +0200

    protocol: add `decrypt_with_keys` and `encrypt_with_keys` methods to `ProtocolEncryption` trait

--- crates/smartvaults-protocol/src/v2/core/mod.rs
+++ crates/smartvaults-protocol/src/v2/core/mod.rs
@@ -1,6 +1,7 @@
 // Copyright (c) 2022-2023 Smart Vaults
 // Distributed under the MIT software license
 
+use nostr::Keys;
 use smartvaults_core::secp256k1::{SecretKey, XOnlyPublicKey};
 
 pub(crate) mod crypto;
@@ -23,6 +24,7 @@ pub trait ProtocolEncryption: ProtocolEncoding
 where
     <Self as ProtocolEncryption>::Err: From<<Self as ProtocolEncoding>::Err>,
     <Self as ProtocolEncryption>::Err: From<CryptoError>,
+    <Self as ProtocolEncryption>::Err: From<nostr::key::Error>,
 {
     type Err;
 
@@ -50,5 +52,19 @@ where
             buf,
             crypto::Version::XChaCha20Poly1305,
         )?)
+    }
+
+    fn decrypt_with_keys<T>(
+        keys: &Keys,
+        payload: T,
+    ) -> Result<Self, <Self as ProtocolEncryption>::Err>
+    where
+        T: AsRef<[u8]>,
+    {
+        Self::decrypt(&keys.secret_key()?, &keys.public_key(), payload)
+    }
+
+    fn encrypt_with_keys(&self, keys: &Keys) -> Result<String, <Self as ProtocolEncryption>::Err> {
+        self.encrypt(&keys.secret_key()?, &keys.public_key())
     }
 }
