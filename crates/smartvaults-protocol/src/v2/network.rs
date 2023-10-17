8b5934f72417a5f99561821d817bc188712385b6
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-10-17 12:19:40 +0200

    protocol: de-impl Serialize and Deserialize for `NetworkMagic`

--- crates/smartvaults-protocol/src/v2/network.rs
+++ crates/smartvaults-protocol/src/v2/network.rs
@@ -3,7 +3,6 @@
 
 use core::ops::Deref;
 
-use serde::{Deserialize, Deserializer, Serialize};
 use smartvaults_core::bitcoin::network::constants::UnknownMagic;
 use smartvaults_core::bitcoin::network::Magic;
 use smartvaults_core::bitcoin::{consensus, Network};
@@ -37,56 +36,14 @@ impl Deref for NetworkMagic {
 }
 
 impl NetworkMagic {
+    pub fn new(network: Network) -> Self {
+        Self::from(network)
+    }
+
     pub fn from_slice(slice: &[u8]) -> Result<Self, Error> {
         let magic: Magic = consensus::deserialize(slice)?;
         Ok(Self {
             inner: Network::try_from(magic)?,
         })
-    }
-}
-
-impl Serialize for NetworkMagic {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: serde::Serializer,
-    {
-        let magic: [u8; 4] = self.magic().to_bytes();
-        serializer.serialize_bytes(&magic)
-    }
-}
-
-impl<'de> Deserialize<'de> for NetworkMagic {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: Deserializer<'de>,
-    {
-        type Bytes = [u8; 4];
-        let bytes: Bytes = Bytes::deserialize(deserializer)?;
-        let magic: Magic = Magic::from_bytes(bytes);
-        let network: Network = Network::try_from(magic).map_err(serde::de::Error::custom)?;
-        Ok(Self { inner: network })
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_network_magic_serialization() {
-        let magic = NetworkMagic::from(Network::Bitcoin);
-        let ser: Vec<u8> = serde_json::to_vec(&magic).unwrap();
-        assert_eq!(&ser, b"[249,190,180,217]");
-    }
-
-    #[test]
-    fn test_network_magic_deserialization() {
-        let magic: &str = "[249,190,180,217]";
-        let network: NetworkMagic = serde_json::from_slice(magic.as_bytes()).unwrap();
-        assert_eq!(network, Network::Bitcoin.into());
-
-        let magic: &str = "[11,17,9,7]";
-        let network: NetworkMagic = serde_json::from_slice(magic.as_bytes()).unwrap();
-        assert_eq!(network, Network::Testnet.into());
     }
 }
