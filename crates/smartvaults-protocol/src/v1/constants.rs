643b1f31755ec21ded010b486ed1a466b29d7962
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-11-06 11:59:59 +0100

    protocol: update mainnet public key

--- crates/smartvaults-protocol/src/v1/constants.rs
+++ crates/smartvaults-protocol/src/v1/constants.rs
@@ -10,8 +10,8 @@ use smartvaults_core::secp256k1::XOnlyPublicKey;
 
 // Smart Vaults Public Keys
 pub static SMARTVAULTS_MAINNET_PUBLIC_KEY: Lazy<XOnlyPublicKey> = Lazy::new(|| {
-    XOnlyPublicKey::from_str("5f5d73eee1b08e1743142538b1acb65ec16c1475b6b6902ca2380b19b6b4c006")
-        .expect("Invalid public key") // TODO: to replace
+    XOnlyPublicKey::from_str("32c961f39afcff6df6abed251b346550329b2dbcabca0667530f0be5054fe7ae")
+        .expect("Invalid public key") // npub1xtykruu6lnlkma4ta5j3kdr92qefktdu409qve6npu972p20u7hqhh99km
 });
 pub static SMARTVAULTS_TESTNET_PUBLIC_KEY: Lazy<XOnlyPublicKey> = Lazy::new(|| {
     XOnlyPublicKey::from_str("2c2dcda12330dda3b9600237a419003c5d9bf3d757303e63ecee121b4aaa2fa0")
