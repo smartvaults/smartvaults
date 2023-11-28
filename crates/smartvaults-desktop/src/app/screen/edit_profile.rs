3c1a54fe3a153f6fc4cc2032400de18e785ab269
Yuki Kishimoto <yukikishimoto@protonmail.com>
2023-11-28 11:37:12 +0100

    gui: allow to save key agent metadata

--- crates/smartvaults-desktop/src/app/screen/edit_profile.rs
+++ crates/smartvaults-desktop/src/app/screen/edit_profile.rs
@@ -4,8 +4,10 @@
 use iced::widget::{Column, Row, Space};
 use iced::{Alignment, Command, Element, Length};
 use smartvaults_sdk::nostr::Metadata;
+use smartvaults_sdk::protocol::v1::KeyAgentMetadata;
 
 use crate::app::component::Dashboard;
+use crate::app::context::Mode;
 use crate::app::{Context, Message, Stage, State};
 use crate::component::{Button, Text, TextInput};
 use crate::theme::color::DARK_RED;
@@ -16,6 +18,10 @@ pub enum EditProfileMessage {
     NameChanged(String),
     DisplayNameChanged(String),
     NIP05Changed(String),
+    JurisdictionChanged(String),
+    TwitterChanged(String),
+    FacebookChanged(String),
+    LinkedinChanged(String),
     ErrorChanged(Option<String>),
     Save,
 }
@@ -28,6 +34,10 @@ pub struct EditProfileState {
     name: String,
     display_name: String,
     nip05: String,
+    jurisdiction: String,
+    twitter: String,
+    facebook: String,
+    linkedin: String,
     error: Option<String>,
 }
 
@@ -81,6 +91,12 @@ impl State for EditProfileState {
                     self.display_name = display_name
                 }
                 EditProfileMessage::NIP05Changed(nip05) => self.nip05 = nip05,
+                EditProfileMessage::JurisdictionChanged(jurisdiction) => {
+                    self.jurisdiction = jurisdiction
+                }
+                EditProfileMessage::TwitterChanged(twitter) => self.twitter = twitter,
+                EditProfileMessage::FacebookChanged(facebook) => self.facebook = facebook,
+                EditProfileMessage::LinkedinChanged(linkedin) => self.linkedin = linkedin,
                 EditProfileMessage::ErrorChanged(error) => {
                     self.error = error;
                     self.loading = false;
@@ -88,7 +104,19 @@ impl State for EditProfileState {
                 EditProfileMessage::Save => {
                     self.loading = true;
                     let client = ctx.client.clone();
-                    let mut metadata = Metadata::new();
+
+                    let mut metadata = match ctx.mode {
+                        Mode::User => Metadata::new(),
+                        Mode::KeyAgent => {
+                            let mut key_agent_metadata = KeyAgentMetadata::default();
+
+                            if !self.jurisdiction.is_empty() {
+                                key_agent_metadata.jurisdiction = Some(self.jurisdiction.clone());
+                            }
+
+                            key_agent_metadata.into_metadata()
+                        }
+                    };
 
                     if !self.name.is_empty() {
                         metadata = metadata.name(&self.name);
@@ -149,7 +177,37 @@ impl State for EditProfileState {
                 .push(Text::new("Edit profile").big().bold().view())
                 .push(name)
                 .push(display_name)
-                .push(nip05)
+                .push(nip05);
+
+            if let Mode::KeyAgent = ctx.mode {
+                let jurisdiction = TextInput::with_label("Jurisdiction", &self.jurisdiction)
+                    .on_input(|s| EditProfileMessage::JurisdictionChanged(s).into())
+                    .placeholder("Jurisdiction")
+                    .view();
+
+                let twitter = TextInput::with_label("X (Twitter)", &self.twitter)
+                    .on_input(|s| EditProfileMessage::TwitterChanged(s).into())
+                    .placeholder("X (Twitter)")
+                    .view();
+
+                let facebook = TextInput::with_label("Facebook", &self.facebook)
+                    .on_input(|s| EditProfileMessage::FacebookChanged(s).into())
+                    .placeholder("Facebook")
+                    .view();
+
+                let linkedin = TextInput::with_label("Linkedin", &self.linkedin)
+                    .on_input(|s| EditProfileMessage::LinkedinChanged(s).into())
+                    .placeholder("Linkedin")
+                    .view();
+
+                content = content
+                    .push(jurisdiction)
+                    .push(twitter)
+                    .push(facebook)
+                    .push(linkedin);
+            }
+
+            content = content
                 .push(error)
                 .push(Space::with_height(Length::Fixed(15.0)))
                 .push(
