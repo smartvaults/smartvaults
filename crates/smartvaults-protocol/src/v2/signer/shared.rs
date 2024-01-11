0de7bbc30c295d2587dda40cf3984ff7831a4903
Yuki Kishimoto <yukikishimoto@protonmail.com>
2024-01-11 15:59:01 +0100

    protocol(v2): impl ProtocolEncoding for SharedSigner

--- crates/smartvaults-protocol/src/v2/signer/shared.rs
+++ crates/smartvaults-protocol/src/v2/signer/shared.rs
@@ -5,12 +5,15 @@
 
 use core::ops::Deref;
 
+use prost::Message;
 use smartvaults_core::crypto::hash;
 use smartvaults_core::secp256k1::XOnlyPublicKey;
 use smartvaults_core::CoreSigner;
 
 use super::SignerIdentifier;
-use crate::v2::NostrPublicIdentifier;
+use crate::v2::core::SchemaVersion;
+use crate::v2::proto::signer::ProtoSharedSigner;
+use crate::v2::{Error, NostrPublicIdentifier, ProtocolEncoding};
 
 /// Shared Signer
 #[derive(Debug, Clone, PartialEq, Eq)]
@@ -63,5 +66,19 @@ impl SharedSigner {
             self.network()
         );
         NostrPublicIdentifier::from(hash::sha256(unhashed))
+    }
+}
+
+impl ProtocolEncoding for SharedSigner {
+    type Err = Error;
+
+    fn pre_encoding(&self) -> (SchemaVersion, Vec<u8>) {
+        let shared_signer: ProtoSharedSigner = self.into();
+        (SchemaVersion::ProtoBuf, shared_signer.encode_to_vec())
+    }
+
+    fn decode_protobuf(data: &[u8]) -> Result<Self, Self::Err> {
+        let shared_signer: ProtoSharedSigner = ProtoSharedSigner::decode(data)?;
+        Self::try_from(shared_signer)
     }
 }
